460663070bafeaa54984604677ff5efa44033cfe
pax-ad <ad007scrutinize@gmail.com>
2026-07-23 00:28:39 +0530

    exportind Directory

---
+++ ExportDrectory/Fruits/apple.js
@@ -0,0 +1,6 @@
+//seeing how the exports work on antoher directory
+
+module.exports = {
+    name: "apple",
+    color: "red"
+};
---
+++ ExportDrectory/Script.js
@@ -0,0 +1,6 @@
+//exporting a directory
+const info = require("./Fruits"); //we are require ing the whole directory.
+
+console.log(info);
+
+console.log(info,0); //printing what is at 0 index
