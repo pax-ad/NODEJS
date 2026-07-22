17e5db0deccb55ada801e330be46d9108ff6b584
pax-ad <ad007scrutinize@gmail.com>
2026-07-23 05:04:18 +0530

    modified

--- MISCELLANEOUS/BACKEND/index.js
+++ MISCELLANEOUS/BACKEND/index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//get and post method
 app.get("/register", (req,res) => {
     let {user,password} = req.query;
     res.send(`welcome ${user}`);
     //res.send("GET response");
 })
 
+//handlling post requests
+
+app.use (express.urlencoded({extended:true}));
+app.use(express.json());
 app.post("/register", (req,res) => {
-    res.send("post response");
+    //console.log(req.body);
+    let {user,password} =req.body;
+    res.send(`post response , welcome ${user}`);
 })
+
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
 });
+
+
