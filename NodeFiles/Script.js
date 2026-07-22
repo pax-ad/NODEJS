1ac8efe33d821efe2e02ea21d2cc4f47785f7084
pax-ad <ad007scrutinize@gmail.com>
2026-07-22 19:51:36 +0530

    Intial commit

---
+++ NodeFiles/Script.js
@@ -0,0 +1,15 @@
+//for running the js file inside node repl
+
+let n = 5 ;
+for(let i = 0 ; i < n ; i++){
+    console.log("hello",i);
+}
+
+//for process
+console.log(process.argv);
+
+//passing the arguments
+let args = process.argv;
+for (let i =0;i<args.length;i++){
+    console.log("hello",args[i]); //args[i] is arguments inside the terminal
+}
