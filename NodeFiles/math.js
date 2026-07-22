b343712baa5a6e0eba3707e40de40f76998d88bc
pax-ad <ad007scrutinize@gmail.com>
2026-07-22 23:20:52 +0530

    module.exports

---
+++ NodeFiles/math.js
@@ -0,0 +1,10 @@
+//for showcasing the module.exports
+
+
+const sum = (a,b) => a+b;
+const mul = (a,b) => a*b;
+
+//suppose we want to use this in another file
+
+//write this
+module.exports = "123";
